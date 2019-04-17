d1a21b330fb705c5d3fece0ef0024b68d3f2d897
umeer <umeermohammad@gmail.com>
2019-04-17 13:36:05 +0200

    implemented chain verification algh

--- routing_umeer/node/App.cc
+++ routing_umeer/node/App.cc
@@ -10,23 +10,32 @@ using namespace omnetpp;
 
 const int INITIAL_MONEY = 10;
 
-class TrustChainElement {
+//TODO LIST
+// - create event logging system
+// - create transaction dissemination algh
+// - expand system to multiple nodes parallel transactions (heavy debug required)
+// - implement evil node
+
+class TrustChainElement
+{
 public:
     int partnerId, partnerSeqNum, transactionValue; // values limited to +- 2147483647
 
-    TrustChainElement(int partnerId, int partnerSeqNum, int transactionValue) {
+    TrustChainElement(int partnerId, int partnerSeqNum, int transactionValue)
+    {
         this->partnerId = partnerId;
         this->partnerSeqNum = partnerSeqNum;
         this->transactionValue = transactionValue;
     }
 };
 
-class LogDatabaseElement {
+class LogDatabaseElement
+{
 public:
     int UserAId, UserASeqNum, UserBId, UserBSeqNum, transactionValue; // values limited to +- 2147483647
 
-    LogDatabaseElement(int UserAId, int UserASeqNum, int UserBId,
-            int UserBSeqNum, int transactionValue) {
+    LogDatabaseElement(int UserAId, int UserASeqNum, int UserBId, int UserBSeqNum, int transactionValue)
+    {
         this->UserAId = UserAId;
         this->UserASeqNum = UserASeqNum;
         this->UserBId = UserBId;
@@ -35,7 +44,8 @@ public:
     }
 };
 
-class App: public cSimpleModule {
+class App : public cSimpleModule
+{
 private:
     // configuration
     int myAddress;
@@ -87,24 +97,27 @@ protected:
     virtual void createAckMessage();
     virtual void createDisseminationMessage();
 
-    virtual void verificationTransactionChain(Packet *pk);
+    virtual bool verificationTransactionChain(Packet *pk);
     virtual void logTransactionChain(Packet *pk);
     virtual bool isAlreadyPresentInDb(LogDatabaseElement *element);
 };
 
 Define_Module(App);
 
-App::App() {
+App::App()
+{
     //generatePacket = nullptr;
     timerThread = nullptr;
 }
 
-App::~App() {
+App::~App()
+{
     //cancelAndDelete(generatePacket);
     cancelAndDelete(timerThread);
 }
 
-void App::initialize() {
+void App::initialize()
+{
     myAddress = par("address");
     packetLengthBytes = &par("packetLength");
     sendIATime = &par("sendIaTime");  // volatile parameter
@@ -128,8 +141,7 @@ void App::initialize() {
         destAddresses.push_back(atoi(token));
 
     if (destAddresses.size() == 0)
-        throw cRuntimeError(
-                "At least one address must be specified in the destAddresses parameter!");
+        throw cRuntimeError("At least one address must be specified in the destAddresses parameter!");
 
     // TrustChain initialization
     registerNewChainNode(-1, -1, INITIAL_MONEY);
@@ -143,15 +155,18 @@ void App::initialize() {
     sourceAddressSignal = registerSignal("sourceAddress");
 }
 
-void App::handleMessage(cMessage *msg) {
+void App::handleMessage(cMessage *msg)
+{
     if (msg == timerThread) {
         threadFunction();
-    } else {
+    }
+    else {
         receiveMessage(msg);
     }
 }
 
-void App::threadFunction() {
+void App::threadFunction()
+{
     if (waitingForAck == 0) { //Not transaction are pending
 
         //Just for Production Test (ONLY 0 CAN SEND MONEY)
@@ -172,56 +187,59 @@ void App::threadFunction() {
     scheduleAt(simTime() + sendIATime->doubleValue(), timerThread);
 }
 
-void App::receiveMessage(cMessage *msg) {
+void App::receiveMessage(cMessage *msg)
+{
     // Handle incoming packet
     Packet *pk = check_and_cast<Packet *>(msg);
 
-    EV << "received packet " << pk->getName() << " after " << pk->getHopCount()
-              << "hops" << endl;
+    //EV << "received packet " << pk->getName() << " after " << pk->getHopCount()<< "hops" << endl;
     emit(endToEndDelaySignal, simTime() - pk->getCreationTime());
     emit(hopCountSignal, pk->getHopCount());
     emit(sourceAddressSignal, pk->getSrcAddr());
 
     switch (pk->getPacketType()) {
-    case 0: { // Transaction Request Received
-        tempBlockID = pk->getSrcAddr();
-        tempPartnerSeqNum = pk->getMyChainSeqNum();
-        tempBlockTransaction = pk->getTransactionValue();
-        createChainRequestMessage();
-        waitingForChainLog = 1;
-        break;
-    }
-    case 1: { // Chain Request Received
-        createChainLogMessage();
-        break;
-    }
-    case 2: { // Partner Chain Received
-        verificationTransactionChain(pk);
-        logTransactionChain(pk);
+        case 0: { // Transaction Request Received
+            tempBlockID = pk->getSrcAddr();
+            tempPartnerSeqNum = pk->getMyChainSeqNum();
+            tempBlockTransaction = pk->getTransactionValue();
+            createChainRequestMessage();
+            waitingForChainLog = 1;
+            break;
+        }
+        case 1: { // Chain Request Received
+            createChainLogMessage();
+            break;
+        }
+        case 2: { // Partner Chain Received
 
-        waitingForChainLog = 0;
-        registerNewChainNode(tempBlockID, tempPartnerSeqNum,
-                tempBlockTransaction);
-        createAckMessage();
-        createDisseminationMessage();
-        tempBlockID = 0;
-        tempBlockTransaction = 0;
-        break;
-    }
-    case 3: { // Ack Received
-        if (pk->getSrcAddr() == tempBlockID) {
-            registerNewChainNode(tempBlockID, pk->getMyChainSeqNum(),
-                    -tempBlockTransaction);
+            if (!verificationTransactionChain(pk)) {
+                char text[128];
+                sprintf(text, "Event number: %lld", getSimulation()->getEventNumber());
+                getSimulation()->getActiveEnvir()->alert(text);
+            }
+            logTransactionChain(pk);
+
+            waitingForChainLog = 0;
+            registerNewChainNode(tempBlockID, tempPartnerSeqNum, tempBlockTransaction);
+            createAckMessage();
             createDisseminationMessage();
             tempBlockID = 0;
             tempBlockTransaction = 0;
-            waitingForAck = 0;
+            break;
         }
-        break;
-    }
-    default: {
-        break;
-    }
+        case 3: { // Ack Received
+            if (pk->getSrcAddr() == tempBlockID) {
+                registerNewChainNode(tempBlockID, pk->getMyChainSeqNum(), -tempBlockTransaction);
+                createDisseminationMessage();
+                tempBlockID = 0;
+                tempBlockTransaction = 0;
+                waitingForAck = 0;
+            }
+            break;
+        }
+        default: {
+            break;
+        }
     }
     delete pk;
 
@@ -229,7 +247,8 @@ void App::receiveMessage(cMessage *msg) {
         getParentModule()->bubble("Arrived!");
 }
 
-void App::createTransactionMessage() {
+void App::createTransactionMessage()
+{
     //Calculate transaction value
     int transactionValue = intuniform(1, chainTotalValue);
 
@@ -240,9 +259,7 @@ void App::createTransactionMessage() {
     tempBlockTransaction = transactionValue;
 
     char pkname[40];
-    sprintf(pkname, "#%ld from-%d-to-%d $%d", pkCounter++, myAddress,
-            destAddress, transactionValue);
-    //EV << "generating packet " << pkname << endl;
+    sprintf(pkname, "#%ld from-%d-to-%d $%d", pkCounter++, myAddress, destAddress, transactionValue);
 
     if (hasGUI())
         getParentModule()->bubble("Generating packet...");
@@ -260,13 +277,12 @@ void App::createTransactionMessage() {
     send(pk, "out");
 }
 
-void App::createChainRequestMessage() {
+void App::createChainRequestMessage()
+{
     int destAddress = tempBlockID;
 
     char pkname[40];
-    sprintf(pkname, "#%ld from-%d-to-%d chain request", pkCounter++, myAddress,
-            destAddress);
-    //EV << "generating packet " << pkname << endl;
+    sprintf(pkname, "#%ld from-%d-to-%d chain request", pkCounter++, myAddress, destAddress);
 
     if (hasGUI())
         getParentModule()->bubble("Generating packet...");
@@ -282,13 +298,12 @@ void App::createChainRequestMessage() {
     send(pk, "out");
 }
 
-void App::createChainLogMessage() {
+void App::createChainLogMessage()
+{
     int destAddress = tempBlockID;
 
     char pkname[40];
-    sprintf(pkname, "#%ld from-%d-to-%d chain log", pkCounter++, myAddress,
-            destAddress);
-    //EV << "generating packet " << pkname << endl;
+    sprintf(pkname, "#%ld from-%d-to-%d chain log", pkCounter++, myAddress, destAddress);
 
     if (hasGUI())
         getParentModule()->bubble("Generating packet...");
@@ -313,7 +328,8 @@ void App::createChainLogMessage() {
             pk->setUserBSeqNum(i, trustChain[i].partnerSeqNum);
             pk->setTransaction(i, trustChain[i].transactionValue);
         }
-    } else {
+    }
+    else {
         pk->setUserBIDArraySize(0);
         pk->setUserBSeqNumArraySize(0);
         pk->setTransactionArraySize(0);
@@ -322,12 +338,12 @@ void App::createChainLogMessage() {
     send(pk, "out");
 }
 
-void App::createAckMessage() {
+void App::createAckMessage()
+{
     int destAddress = tempBlockID;
 
     char pkname[40];
-    sprintf(pkname, "#%ld from-%d-to-%d transaction ack", pkCounter++,
-            myAddress, destAddress);
+    sprintf(pkname, "#%ld from-%d-to-%d transaction ack", pkCounter++, myAddress, destAddress);
 
     if (hasGUI())
         getParentModule()->bubble("Generating packet...");
@@ -344,67 +360,98 @@ void App::createAckMessage() {
     send(pk, "out");
 }
 
-void App::verificationTransactionChain(Packet *pk) {
-    if (pk->getUserBIDArraySize() != 0) {
-        int i;
-        for (i = 0; i < pk->getUserBIDArraySize(); i++) {
-            EV << " User B array size: " << pk->getUserBIDArraySize()
-                      << " get user: " << pk->getUserBID(i)
-                      << " get user chain seq numb: " << pk->getUserBSeqNum(i)
-                      << " get transaction: " << pk->getTransaction(i) << endl;
+bool App::verificationTransactionChain(Packet *pk)
+{
+
+    //check if the your local information can be compared to the received informations
+    // 1 - same sequence number and user ids number have the same transaction value (chain block replaced with another one)
+    // 2 - All the local info regarding a user can be found in the chain (not hiding something)
+
+    int i;
+    for (i = 0; i < logDatabase.size(); i++) {
+
+        if (logDatabase[i].UserAId == pk->getSrcAddr()) {
+            int index = logDatabase[i].UserASeqNum - 1;
+            if (pk->getUserBIDArraySize() > index) {
+                if (pk->getUserBID(index) == logDatabase[i].UserBId && pk->getUserBSeqNum(index) == logDatabase[i].UserBSeqNum && pk->getTransaction(index) == logDatabase[i].transactionValue) {
+                    //all good!
+                }
+                else {
+                    EV << "****The local transaction is not matching with the data provided" << endl;
+                    return false;
+                }
+            }
+            else {
+                EV << "****There aren't enough transactions in the received chain" << endl;
+                return false;
+            }
         }
+        else if (logDatabase[i].UserBId == pk->getSrcAddr()) {
+            int index = logDatabase[i].UserBSeqNum - 1;
+            if (pk->getUserBIDArraySize() > index) {
+                if (pk->getUserBID(index) == logDatabase[i].UserAId && pk->getUserBSeqNum(index) == logDatabase[i].UserASeqNum && pk->getTransaction(index) == -logDatabase[i].transactionValue) {
+                    //all good!
+                }
+                else {
+                    EV << "****The local transaction is not matching with the data provided B" << endl;
+                    return false;
+                }
+            }
+            else {
+                EV << "****There aren't enough transactions in the received chain B" << endl;
+                return false;
+            }
+
+        }
+
     }
+
+    return true;
 }
 
-void App::logTransactionChain(Packet *pk) {
+void App::logTransactionChain(Packet *pk)
+{
     if (pk->getUserBIDArraySize() != 0) {
         int i;
         for (i = 1; i < pk->getUserBIDArraySize(); i++) {
-            LogDatabaseElement *element = new LogDatabaseElement(
-                    pk->getSrcAddr(), i + 1, pk->getUserBID(i),
-                    pk->getUserBSeqNum(i), pk->getTransaction(i));
+            LogDatabaseElement *element = new LogDatabaseElement(pk->getSrcAddr(), i + 1, pk->getUserBID(i), pk->getUserBSeqNum(i), pk->getTransaction(i));
             if (!isAlreadyPresentInDb(element)) {
                 logDatabase.push_back(*element);
-                EV << " **************new element" << endl;
-            } else {
-                EV << " ***************already present " << endl;
+                //EV << " **************new element" << endl;
+            }
+            else {
+                //EV << " ***************already present " << endl;
             }
         }
     }
 }
 
-bool App::isAlreadyPresentInDb(LogDatabaseElement *element) {
+bool App::isAlreadyPresentInDb(LogDatabaseElement *element)
+{
     int i;
     for (i = 0; i < logDatabase.size(); i++) {
-        if (logDatabase[i].UserAId == element->UserAId
-                && logDatabase[i].UserASeqNum == element->UserASeqNum
-                && logDatabase[i].UserBId == element->UserBId
-                && logDatabase[i].UserBSeqNum == element->UserBSeqNum) {
+        if (logDatabase[i].UserAId == element->UserAId && logDatabase[i].UserASeqNum == element->UserASeqNum && logDatabase[i].UserBId == element->UserBId && logDatabase[i].UserBSeqNum == element->UserBSeqNum) {
             return true;
         }
-        if (logDatabase[i].UserAId == element->UserBId
-                && logDatabase[i].UserASeqNum == element->UserBSeqNum
-                && logDatabase[i].UserBId == element->UserAId
-                && logDatabase[i].UserBSeqNum == element->UserASeqNum) {
+        if (logDatabase[i].UserAId == element->UserBId && logDatabase[i].UserASeqNum == element->UserBSeqNum && logDatabase[i].UserBId == element->UserAId && logDatabase[i].UserBSeqNum == element->UserASeqNum) {
             return true;
         }
     }
     return false;
 }
 
-void App::createDisseminationMessage() {
+void App::createDisseminationMessage()
+{
     //TODO
     int i;
     for (i = 0; i < logDatabase.size(); i++) {
-        EV << " UserA " << logDatabase[i].UserAId << " UserADeqNum: "
-                  << logDatabase[i].UserASeqNum << " UserB "
-                  << logDatabase[i].UserBId << " UserBDeqNum: "
-                  << logDatabase[i].UserBSeqNum << " transaction:"
-                  << logDatabase[i].transactionValue << endl;
+        EV << " UserA " << logDatabase[i].UserAId << " UserADeqNum: " << logDatabase[i].UserASeqNum << " UserB " << logDatabase[i].UserBId << " UserBDeqNum: " << logDatabase[i].UserBSeqNum << " transaction:"
+                << logDatabase[i].transactionValue << endl;
     }
 }
 
-int App::randomNodeAddressPicker() {
+int App::randomNodeAddressPicker()
+{
     int destAddress = -1;
 
     while (destAddress == -1 || destAddress == myAddress) {
@@ -414,24 +461,24 @@ int App::randomNodeAddressPicker() {
     return destAddress;
 }
 
-void App::registerNewChainNode(int id, int seqNum, int value) {
+void App::registerNewChainNode(int id, int seqNum, int value)
+{
     TrustChainElement *chainElement = new TrustChainElement(id, seqNum, value);
     trustChain.push_back(*chainElement);
     calculateChainValue();
 
     if (id != -1) {
-        LogDatabaseElement *element = new LogDatabaseElement(myAddress,
-                trustChain.size(), id, seqNum, value);
+        LogDatabaseElement *element = new LogDatabaseElement(myAddress, trustChain.size(), id, seqNum, value);
         logDatabase.push_back(*element);
     }
 }
 
-void App::calculateChainValue() {
+void App::calculateChainValue()
+{
     int localTotalChainValue = 0, i = 0;
 
     for (i = 0; i < trustChain.size(); i++) {
-        localTotalChainValue = localTotalChainValue
-                + trustChain[i].transactionValue;
+        localTotalChainValue = localTotalChainValue + trustChain[i].transactionValue;
     }
     chainTotalValue = localTotalChainValue;
 }
