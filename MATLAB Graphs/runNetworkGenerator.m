7dd57980e5b6b5bd9fbb852baf8d4fa493372197
umeer <umeermohammad@gmail.com>
2019-07-02 20:41:37 +0200

    updated MATLAB graph rappresentation (now with density colour map)

--- MATLAB Graphs/runNetworkGenerator.m
+++ MATLAB Graphs/runNetworkGenerator.m
@@ -1,11 +1,21 @@
-h2 = WattsStrogatz(500,2,0.06);
+graph = WattsStrogatz(500,2,0.05);
 
 disp("AvgPathLength:");
-disp(mean(mean(distances(h2))));
+disp(mean(mean(distances(graph))));
 disp("VariancePathLength: ");
-disp(mean(var(distances(h2))));
+disp(mean(var(distances(graph))));
 
+figure('name','Graph with $N = 500$ nodes');
 
-plot(h2,'NodeColor','k','EdgeAlpha',0.1);
-title('Watts-Strogatz Graph with $N = 500$ nodes, $K = 25$, and $\beta = 0.15$', ...
-    'Interpreter','latex')
+colormap hsv
+deg = degree(graph);
+nSizes = 2*sqrt(deg-min(deg)+0.2);
+nColors = deg;
+plot(graph,'MarkerSize',nSizes,'NodeCData',nColors,'EdgeAlpha',0.1)
+title('Watts-Strogatz Graph with $N = 500$ nodes, $K = 25$, and $\beta = 0.15$','Interpreter','latex')
+colorbar
+
+clear ans
+clear deg
+clear nSizes
+clear nColors
